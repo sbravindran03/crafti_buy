2fcda76e5fa9f30105a5e3c6d3e778b28986ed87
sbravindran03 <ravindran5211@gmail.com>
2023-04-30 10:29:49 +0530

    inital commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,20 +6,19 @@ authRouter.post('/api/signup',async (req,res)=>{
     const{name,email,password}= req.body;
     const existingUser =await User.findone({email});
     const existingSeller = await seller.findone({email});
-    if(existingSeller){
-        return res.json({msg:'User with same email already exist'})
-
-    }
+ 
     if(existingUser){
+        return res.status(400).json({msg:"User with same email already exist"});
         
     }
+    // 200
     // {
         // 'name:name,'email':email, 'password':password
     // } 1:26
     //  weak-password -6 charcters ,same -account -with  email
 
     // get the data from the client 
-    // post that data in database to the user
+    // post that data in dat abase to the user
 
 });
 module.exports=authRouter;
