654083d1dfcc1d70dbb770c188229692285f325a
sbravindran03 <ravindran5211@gmail.com>
2023-05-07 14:31:14 +0530

    update

--- server/auth_service.dart
+++ server/auth_service.dart
@@ -1,3 +1,10 @@
 class AuthService {
   // sign up user
+  void signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {} catch (e) {}
+  }
 }
